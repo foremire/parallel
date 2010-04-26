61096f8b4a834f47ac1bd72c7bfa925abb8c1283
Xitong Liu <foremire@gmail.com>
2010-04-26 11:45:52 -0400

    remote passed-in parameters for further optimization under certain matrix size

--- project/src/mm.c
+++ project/src/mm.c
@@ -6,39 +6,13 @@
 #include <sys/time.h>
 #include <omp.h>
 #include <papi.h>
+#include "common.h"
 
-// macro
-#define OUTPUT_THRESHOLD 7
-#define TRUE 1
-#define FALSE 0
-
-#define SAFE_FREE(addr)\
-  if(NULL != addr){\
-    free(addr);\
-    addr = NULL;\
-  }
-
-//global varialbes and definitions
-typedef struct _matrix{
-  double * data;
-  int xDim;
-  int yDim;
-}matrix;
-
-// commen messages
-char * usage = "Usage: mm P N\n";
+// messages
 char * thread_num_error = "Invalid thread number P = %d. It must be greater than 0.\n";
 char * matrix_size_error = "Invalid matrix size N = %d. It must be greater than 1.\n";
 char * matrix_size_info = "Matrix Size N = %d\n";
 char * malloc_error = "malloc() ERROR.\n";
-
-// function declaration
-void init_matrix(matrix * mat, int xDim, int yDim, int random);
-void omp_matrix_mul(matrix matrixA, matrix matrixB, matrix matrixC, int thread_num);
-void serial_matrix_mul(matrix matrixA, matrix matrixB, matrix matrixC);
-void print_matrix(matrix mat);
-double get_duration(struct timeval __start);
-void validate_result(matrix matrixA, matrix matrixB);
 
 int main( int argc, char *argv[] )
 {
@@ -47,31 +21,7 @@ int main( int argc, char *argv[] )
   matrix matrixC;
   matrix matrixCValid;
 
-  int matrix_size = 0;
-  int thread_num = 1;
-
-  if(argc < 3){
-    puts(usage);
-    exit(-1);
-  }
-  
-  // get the matrix from the command line options list
-  thread_num = atoi(argv[1]);
-  if(0 > thread_num){
-    printf(thread_num_error, thread_num);
-    exit(-1);
-  }
-
-  matrix_size = atoi(argv[2]);
-  if(1 > matrix_size){
-    printf(matrix_size_error, matrix_size);
-    exit(-1);
-  }
- 
-  // make sure the thread_num is always less than or euqal to matrix_size
-  if(thread_num > matrix_size){
-    thread_num = matrix_size;
-  }
+  int matrix_size = MATRIX_SIZE;
 
   // initialize PAPI
   int ret = PAPI_library_init( PAPI_VER_CURRENT );
@@ -106,7 +56,7 @@ int main( int argc, char *argv[] )
 
   // do it in parallel way
   PAPI_start(EventSet);
-  omp_matrix_mul(matrixA, matrixB, matrixC, thread_num);
+  omp_matrix_mul(matrixA, matrixB, matrixC);
   PAPI_stop(EventSet, values);
 
   double cycles = ( double ) values[ 0 ];
@@ -174,7 +124,7 @@ void init_matrix(matrix * mat, int xDim, int yDim, int random){
   }
 }
 
-void omp_matrix_mul(matrix matrixA, matrix matrixB, matrix matrixC, int thread_num){
+void omp_matrix_mul(matrix matrixA, matrix matrixB, matrix matrixC){
   int dim = 0;
 
   // check whether the dimension of the three matrices match 
@@ -190,49 +140,25 @@ void omp_matrix_mul(matrix matrixA, matrix matrixB, matrix matrixC, int thread_n
     return;
   }
 
-  
   int thread_id = 0;
-
-  
+  int num_per_thread = dim / PROCESSOR_NUM;
+   
   // calculate the sum in parallel
   //omp_set_num_threads(thread_num);
-  #pragma omp parallel shared (matrixA, matrixB, matrixC)\
+  #pragma omp parallel shared (matrixA, matrixB, matrixC, num_per_thread)\
     private (thread_id)
   {
-    int num_per_thread = 0;
-    int remainder = 0;
-    int range_start = 0;
-    int range_end = 0;
-    int range_len = 0;
-
-    // get the thread id
     thread_id = omp_get_thread_num();
-    
-    // Make sure the data is evenly distributed between the processes
-    num_per_thread = dim / thread_num;
-    remainder = dim % thread_num;
-    if(0 == remainder){
-      range_start = thread_id * num_per_thread;
-    }else{
-      range_start = thread_id * (num_per_thread + 1);
-      range_start = (thread_id > remainder) ? range_start - thread_id + remainder 
-        : range_start;
-    }
-    range_end = (thread_id > remainder - 1) ? range_start + num_per_thread
-      : range_start + num_per_thread + 1;
-    range_len = range_end - range_start;
-
-    //printf("thread_id: %d, range_start: %d, range_end: %d\n", 
-    //    thread_id, range_start, range_end);
+    int start = num_per_thread * thread_id;
+    int end = start + num_per_thread;
+    //printf("TID: %d, %d - %d\n", thread_id, start, end);
   
     int cycleI = 0;
     int cycleJ = 0;
     int cycleK = 0;
-    
-    //#pragma omp for
-    for(cycleI = range_start; cycleI < range_end; ++ cycleI){
-      for(cycleJ = 0; cycleJ < matrixB.xDim; ++ cycleJ){
 
+    for(cycleI = start; cycleI < end; ++ cycleI){
+      for(cycleJ = 0; cycleJ < matrixB.xDim; ++ cycleJ){
         matrixC.data[cycleI * dim + cycleJ] = 0.0;
 
         for(cycleK = 0; cycleK < dim; ++ cycleK){
