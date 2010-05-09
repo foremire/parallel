8301862fca2610b88a93a79194082e4b736e94c6
Xitong Liu <foremire@gmail.com>
2010-05-08 22:07:30 -0400

    add more comment

--- project/src/omp_mat_mul_sse_ppl_asm.s
+++ project/src/omp_mat_mul_sse_ppl_asm.s
@@ -174,42 +174,48 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	cmpl	-24(%rbp), %eax         #cycleK < (dim - SSE_LENGTH + 1)
 	jg	.L12
 	
-        leaq	-112(%rbp), %rax
+        #__builtin_ia32_storeups(imd_ret_0, acc_0);
+        leaq	-112(%rbp), %rax        #oprand_b_0
 	movlps	-48(%rbp), %xmm0
 	movhps	-40(%rbp), %xmm0
 	movups	%xmm0, (%rax)
-	
-        leaq	-128(%rbp), %rax
+
+        #__builtin_ia32_storeups(imd_ret_1, acc_1);
+        leaq	-128(%rbp), %rax        #oprand_b_1
 	movlps	-64(%rbp), %xmm0
 	movhps	-56(%rbp), %xmm0
 	movups	%xmm0, (%rax)
 	
-        leaq	-144(%rbp), %rax
+        #__builtin_ia32_storeups(imd_ret_2, acc_2);
+        leaq	-144(%rbp), %rax        #oprand_b_2
 	movlps	-80(%rbp), %xmm0
 	movhps	-72(%rbp), %xmm0
 	movups	%xmm0, (%rax)
 	
-        leaq	-160(%rbp), %rax
+        #__builtin_ia32_storeups(imd_ret_3, acc_3);
+        leaq	-160(%rbp), %rax        #oprand_b_3
 	movlps	-96(%rbp), %xmm0
 	movhps	-88(%rbp), %xmm0
 	movups	%xmm0, (%rax)
 	
         movl	$1, -24(%rbp)           #++cycleK
 .L15:
-	cmpl	$3, -24(%rbp)
+	cmpl	$3, -24(%rbp)           ##cycleK < (dim - SSE_LENGTH + 1)
 	jle	.L13
+        
 	movq	-248(%rbp), %rax
 	movq	24(%rax), %rax
 	movq	(%rax), %rdx
 	movq	-248(%rbp), %rax
-	movl	32(%rax), %eax
-	imull	-16(%rbp), %eax
-	addl	-20(%rbp), %eax
+	movl	32(%rax), %eax          #dim
+	imull	-16(%rbp), %eax         #cycleI * dim
+	addl	-20(%rbp), %eax         #cycleI * dim + cycleK
 	cltq
 	salq	$2, %rax
 	addq	%rax, %rdx
 	movl	-112(%rbp), %eax
 	movl	%eax, (%rdx)
+
 	movq	-248(%rbp), %rax
 	movq	24(%rax), %rax
 	movq	(%rax), %rdx
@@ -223,6 +229,7 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	addq	%rax, %rdx
 	movl	-128(%rbp), %eax
 	movl	%eax, (%rdx)
+
 	movq	-248(%rbp), %rax
 	movq	24(%rax), %rax
 	movq	(%rax), %rdx
@@ -236,6 +243,7 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	addq	%rax, %rdx
 	movl	-144(%rbp), %eax
 	movl	%eax, (%rdx)
+
 	movq	-248(%rbp), %rax
 	movq	24(%rax), %rax
 	movq	(%rax), %rdx
@@ -252,30 +260,34 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	addl	$4, -20(%rbp)
 	jmp	.L14
 .L13:
-	movss	-112(%rbp), %xmm1
-	movl	-24(%rbp), %eax
-	cltq
+	movss	-112(%rbp), %xmm1       #oprand_b_0
+	movl	-24(%rbp), %eax         #cycleK
+	cltq                            #%eax->%rax
 	movss	-112(%rbp,%rax,4), %xmm0
 	addss	%xmm1, %xmm0
 	movss	%xmm0, -112(%rbp)
+
 	movss	-128(%rbp), %xmm1
 	movl	-24(%rbp), %eax
 	cltq
 	movss	-128(%rbp,%rax,4), %xmm0
 	addss	%xmm1, %xmm0
 	movss	%xmm0, -128(%rbp)
+
 	movss	-144(%rbp), %xmm1
 	movl	-24(%rbp), %eax
 	cltq
 	movss	-144(%rbp,%rax,4), %xmm0
 	addss	%xmm1, %xmm0
 	movss	%xmm0, -144(%rbp)
+
 	movss	-160(%rbp), %xmm1
 	movl	-24(%rbp), %eax
 	cltq
 	movss	-160(%rbp,%rax,4), %xmm0
 	addss	%xmm1, %xmm0
 	movss	%xmm0, -160(%rbp)
+
 	addl	$1, -24(%rbp)
 	jmp	.L15
 .L12:
@@ -291,7 +303,8 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	leaq	(%rdx,%rax), %rax
 	movups	(%rax), %xmm0
 	movlps	%xmm0, -176(%rbp)
-	movhps	%xmm0, -168(%rbp)
+	movhps	%xmm0, -168(%rbp)       #%xmm0 -> oprand_a
+
 	movq	-248(%rbp), %rax
 	movq	16(%rax), %rax
 	movq	(%rax), %rdx
@@ -304,7 +317,8 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	leaq	(%rdx,%rax), %rax
 	movups	(%rax), %xmm0
 	movlps	%xmm0, -192(%rbp)
-	movhps	%xmm0, -184(%rbp)
+	movhps	%xmm0, -184(%rbp)       #%xmm0 -> oprand_b_0
+
 	movq	-248(%rbp), %rax
 	movq	16(%rax), %rax
 	movq	(%rax), %rdx
@@ -319,7 +333,8 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	leaq	(%rdx,%rax), %rax
 	movups	(%rax), %xmm0
 	movlps	%xmm0, -208(%rbp)
-	movhps	%xmm0, -200(%rbp)
+	movhps	%xmm0, -200(%rbp)       #%xmm0 -> oprand_b_1
+
 	movq	-248(%rbp), %rax
 	movq	16(%rax), %rax
 	movq	(%rax), %rdx
@@ -334,22 +349,25 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	leaq	(%rdx,%rax), %rax
 	movups	(%rax), %xmm0
 	movlps	%xmm0, -224(%rbp)
-	movhps	%xmm0, -216(%rbp)
+	movhps	%xmm0, -216(%rbp)       #%xmm0 -> oprand_b_2
+
 	movq	-248(%rbp), %rax
 	movq	16(%rax), %rax
 	movq	(%rax), %rdx
-	movl	-20(%rbp), %eax
-	leal	3(%rax), %ecx
+	movl	-20(%rbp), %eax         #cycleJ
+	leal	3(%rax), %ecx           #cycleJ + 3
 	movq	-248(%rbp), %rax
-	movl	32(%rax), %eax
-	imull	%ecx, %eax
-	addl	-24(%rbp), %eax
+	movl	32(%rax), %eax          #dim
+	imull	%ecx, %eax              #(cycleJ + 3)*dim
+	addl	-24(%rbp), %eax         #(cycleJ + 3)*dim + cycleK
 	cltq
-	salq	$2, %rax
+	salq	$2, %rax                #&(matrixBT.data[(cycleJ + 1) * dim + cycleK]
 	leaq	(%rdx,%rax), %rax
-	movups	(%rax), %xmm0
+	movups	(%rax), %xmm0           #oprand_b_3 -> %xmm0
 	movlps	%xmm0, -240(%rbp)
-	movhps	%xmm0, -232(%rbp)
+	movhps	%xmm0, -232(%rbp)       #%xmm0 -> oprand_b_3
+
+        #acc_0 = __builtin_ia32_addps(acc_0, __builtin_ia32_mulps(oprand_a, oprand_b_0));
 	xorps	%xmm0, %xmm0
 	movlps	-192(%rbp), %xmm0
 	movhps	-184(%rbp), %xmm0
@@ -360,6 +378,8 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	addps	-48(%rbp), %xmm0
 	movlps	%xmm0, -48(%rbp)
 	movhps	%xmm0, -40(%rbp)
+
+        #acc_1 = __builtin_ia32_addps(acc_0, __builtin_ia32_mulps(oprand_a, oprand_b_1));
 	xorps	%xmm0, %xmm0
 	movlps	-208(%rbp), %xmm0
 	movhps	-200(%rbp), %xmm0
@@ -370,6 +390,8 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	addps	-64(%rbp), %xmm0
 	movlps	%xmm0, -64(%rbp)
 	movhps	%xmm0, -56(%rbp)
+
+        #acc_2 = __builtin_ia32_addps(acc_0, __builtin_ia32_mulps(oprand_a, oprand_b_2));
 	xorps	%xmm0, %xmm0
 	movlps	-224(%rbp), %xmm0
 	movhps	-216(%rbp), %xmm0
@@ -380,6 +402,8 @@ omp_mat_mul_sse_ppl_asm.omp_fn.0:
 	addps	-80(%rbp), %xmm0
 	movlps	%xmm0, -80(%rbp)
 	movhps	%xmm0, -72(%rbp)
+
+        #acc_3 = __builtin_ia32_addps(acc_0, __builtin_ia32_mulps(oprand_a, oprand_b_3));
 	xorps	%xmm0, %xmm0
 	movlps	-240(%rbp), %xmm0
 	movhps	-232(%rbp), %xmm0
